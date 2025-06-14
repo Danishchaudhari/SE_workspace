ea5cd3929e755d54714715797e609491e0f4c5f6
Danishchaudhari <danishchaudhri022@gmail.com>
2025-06-14 13:49:48 +0530

    Update main (6).c

--- C Pograming/13-6/main (6).c
+++ C Pograming/13-6/main (6).c
@@ -1,6 +1,6 @@
-#include<stdio.h>//header file
-#include<conio.h>//header file
-void main()//main function
+#include<stdio.h>
+#include<conio.h>
+void main()
 {
     
    //0.5*a*b
@@ -12,5 +12,5 @@ void main()//main function
   
   
     
-    getch();//terminate the program
+    getch();
 }    
