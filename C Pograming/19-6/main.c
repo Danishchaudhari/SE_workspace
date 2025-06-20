6ec23aa441ad5134419c88f1d7a20805117e3276
Danishchaudhari <danishchaudhri022@gmail.com>
2025-06-20 08:46:16 +0530

    Add files via upload

---
+++ C Pograming/19-6/main.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+ void main()
+ {
+     
+    int marks; 
+     
+     printf("enter your marks");
+     scanf("%d",&marks);
+     
+     if(marks>33)
+     {
+        printf("pass");
+     }
+     else
+     {
+       printf("fail");     
+     }
+     
+     getch();
+ }
+ 
