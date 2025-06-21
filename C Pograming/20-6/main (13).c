3a827070bbc835804ab93de91903d36792f37826
Danishchaudhari <danishchaudhri022@gmail.com>
2025-06-21 08:52:24 +0530

    Add files via upload

---
+++ C Pograming/20-6/main (13).c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    
+    int a = 6;
+    int b = 3;
+    
+    a+=b;
+        printf("\n %d",a);
+    
+    a-=b;
+    printf("\n %d",a);
+    
+    a*=b;
+         printf("\n %d",a);
+    
+    
+    getch();
+}
