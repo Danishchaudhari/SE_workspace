3fbb79ed19fa4d3139931b8d126e785fb0eaf3fe
Danishchaudhari <danishchaudhri022@gmail.com>
2025-06-25 11:39:22 +0530

    Add files via upload

---
+++ C Pograming/24-6/task/main (43).c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+ void main()
+ {  
+  
+    int i, j, spaces, num;
+     
+     printf("Enter your num");
+     scanf("%d",&num);
+
+    for(i = 1; i <= num; i++)  
+    {
+        
+        for(j = 1; j <= num - i; j++) 
+        {
+            printf(" ");
+        }
+        
+        for(j = 1; j <= i; j++) 
+        {
+            printf("%d",i);
+        }
+        printf("\n");
+    }
+
+     getch();
+     
+ }
